7b8d62582cd94958ac062ada5e445a19c19aa323
DuRuofu <duruofu@qq.com>
2023-07-09 21:01:41 +0800

    修改debug模块

--- STM32code/User/USART/debug.h
+++ STM32code/User/USART/debug.h
@@ -5,18 +5,19 @@
 #define __DEBUG_H
 
 
-#define DEBUG
-
+#define DEBUG  // 定义 DEBUG 宏，用于控制调试输出
 #ifdef DEBUG
+    // 如果 DEBUG 宏已定义，则定义以下宏用于调试输出
     #define DEBUG_printf(format, ...) printf(format "\r\n", ##__VA_ARGS__)
-    #define DEBUG_info(tag,format, ...) printf("DEBUG_info["tag"]:" format "\r\n", ##__VA_ARGS__)
-    #define DEBUG_warnig(tag,format, ...) printf("DEBUG_warnig["tag"]:" format "\r\n", ##__VA_ARGS__)
-    #define DEBUG__error(tag,format, ...) printf("DEBUG__error["tag"]:" format "\r\n",##__VA_ARGS__)
+    #define DEBUG_info(tag, format, ...) printf("DEBUG_info[" tag "]:" format "\r\n", ##__VA_ARGS__)
+    #define DEBUG_warnig(tag, format, ...) printf("DEBUG_warnig[" tag "]:" format "\r\n", ##__VA_ARGS__)
+    #define DEBUG__error(tag, format, ...) printf("DEBUG__error[" tag "]:" format "\r\n",##__VA_ARGS__)
 #else
+    // 如果 DEBUG 宏未定义，则定义以下宏为空，以屏蔽调试输出
     #define DEBUG_printf(format, ...) printf(format "\r\n", ##__VA_ARGS__)
-    #define DEBUG_info(tag,format, ...) 
-    #define DEBUG_warnig(tag,format, ...)
-    #define DEBUG__error(tag,format, ...) 
+    #define DEBUG_info(tag, format, ...)
+    #define DEBUG_warnig(tag, format, ...)
+    #define DEBUG__error(tag, format, ...)
 #endif
 
 //串口1接收中断初始化
